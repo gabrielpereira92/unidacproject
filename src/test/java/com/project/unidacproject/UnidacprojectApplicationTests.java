ec3d4e4ffdf04f0623302c54a31ddaf4739a61e6
Gabriel Pereira <gabriel_rfp@outlook.com>
2020-12-23 14:58:08 -0300

    First commit

---
+++ src/test/java/com/project/unidacproject/UnidacprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.unidacproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidacprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
